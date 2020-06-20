831ca333cf5eee3ac86ff9866a0ad986c1c4f09b
krutikasawarkar <krutikasawarkar21@gmail.com>
2020-06-20 18:22:40 +0530

    Initial commit.

---
+++ PlotlyDemo/wwwroot/Scripts/HelloWorld.js
@@ -0,0 +1,10 @@
+﻿function plottester() {
+    TESTER = document.getElementById('tester');
+
+    Plotly.plot(TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16]
+    }], {
+        margin: { t: 0 }
+    }, { showSendToCloud: true });
+}
