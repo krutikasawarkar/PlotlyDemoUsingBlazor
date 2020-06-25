553fbeaddff38acca043d36d1bffc98aac662e62
krutikasawarkar <krutikasawarkar21@gmail.com>
2020-06-25 10:44:31 +0530

    trying to pass dotnet object to js.

--- PlotlyDemo/wwwroot/Scripts/HelloWorld.js
+++ PlotlyDemo/wwwroot/Scripts/HelloWorld.js
@@ -1,4 +1,6 @@
-﻿function plottester(data1, data2) {
+﻿var blazorInterop = blazorInterop || {};
+
+blazorInterop.plottester = function plottester(data1, data2) {
     TESTER = document.getElementById('tester');
 
     Plotly.plot(TESTER, [{
@@ -8,3 +10,11 @@
         margin: { t: 0 }
     }, { showSendToCloud: true });
 }
+
+
+blazorInterop.logToConsoleTable = function (obj) {
+    console.table(obj);
+}
+
+////just an example of how to access this fucntion
+//window.blazorInterop.plottester   
